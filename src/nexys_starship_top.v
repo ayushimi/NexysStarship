47b3787cadfebe2252a872f83150ea9cdd23d076
ayushimi <ayushimi@usc.edu>
2021-04-26 19:55:47 -0700

    cleaned up top file - still need to remove BtnRs

--- src/nexys_starship_top.v
+++ src/nexys_starship_top.v
@@ -1,18 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////////
-// Author:			Shideh Shahidi, Bilal Zafar, Gandhi Puvvada
-// Create Date:		02/25/08
-// File Name:		ee354_GCD_top.v 
-// Description: 
-//
-//
-// Revision: 		2.2
-// Additional Comments: 
-// 10/13/2008 debouncing and single_clock_wide pulse_generation modules are added by Gandhi
-// 10/13/2008 Clock Enable (CEN) has been added by Gandhi
-//  3/ 1/2010 The Spring 2009 debounce design is replaced by the Spring 2010 debounce design
-//            Now, in part 2 of the GCD lab, we do single-stepping 
-//  2/19/2012 Nexys-2 to Nexys-3 conversion done by Gandhi
-//  02/20/2020 Nexys-3 to Nexys-4 conversion done by Yue (Julien) Niu
+// Author:			Ayushi Mittal, Kelly Chan
+// Create Date:   	04/10/21
+// File Name:		nexys_starship_top.v 
+// Description: 	Top file for Nexys Starship (EE 354 Final Project).
 //////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns / 1ps
@@ -27,10 +17,10 @@ module nexys_starship_top
 		Sw3, Sw2, Sw1, Sw0, // 8 switches
 		Ld7, Ld6, Ld5, Ld4, Ld3, Ld2, Ld1, Ld0, // 8 LEDs
 		An3, An2, An1, An0,			       // 4 anodes
-		An7, An6, An5, An4,                // another 4 anodes which are not used
+		An7, An6, An5, An4,                // another 4 anodes
 		Ca, Cb, Cc, Cd, Ce, Cf, Cg,        // 7 cathodes
 		Dp,                                // Dot Point Cathode on SSDs
-	    hSync, vSync, vgaR, vgaG, vgaB     // vga 
+	    hSync, vSync, vgaR, vgaG, vgaB     // VGA
 	  );
 
 	/*  INPUTS */
@@ -44,7 +34,6 @@ module nexys_starship_top
 	/*  OUTPUTS */
 	// Control signals on Memory chips 	(to disable them)
 	output 	MemOE, MemWR, RamCS, QuadSpiFlashCS;
-	// Project Specific Outputs
 	// LEDs
 	output 	Ld0, Ld1, Ld2, Ld3, Ld4, Ld5, Ld6, Ld7;
 	// SSD Outputs
@@ -62,8 +51,7 @@ module nexys_starship_top
 	wire        sysClk;
 	wire [2:0] 	ssdscan_clk;
 	reg [26:0]	DIV_CLK;
-	wire move_clk; // slower vga
-	wire timer_clk, game_timer_clk;
+	wire move_clk, timer_clk, game_timer_clk;
 	
 	// VGA wires
 	wire bright;
@@ -71,12 +59,12 @@ module nexys_starship_top
 	wire [11:0] rgb;
 	wire up, down, left, right;
 
-
-	// SM wires
+	// Button pulses
 	wire Start_Ack_Pulse;
 	wire Up_Pulse, Down_Pulse, Left_Pulse, Right_Pulse, Center_Pulse;
-	wire BtnU_Pulse_VGA,Up_Pulse_VGA;
 	wire BtnR_Pulse, BtnU_Pulse, BtnL_Pulse, BtnD_Pulse, BtnC_Pulse;
+	
+	// States
 	wire q_Init, q_Play, q_GameOver; 
 	wire q_TR_Init, q_TR_Working, q_TR_Repair;
 	wire q_BR_Init, q_BR_Working, q_BR_Repair;
@@ -87,35 +75,42 @@ module nexys_starship_top
 	wire q_LM_Init, q_LM_Empty, q_LM_Full; 
 	wire q_RM_Init, q_RM_Empty, q_RM_Full; 
 	
-	// TODO: add game_timer reg 
-	wire play_flag, game_over;
-	wire top_broken, btm_broken, left_broken, right_broken;
+	// SM and VGA flags
+	wire play_flag;
 	wire top_monster_sm, top_monster_vga; 
 	reg top_monster_ctrl; 
 	wire btm_monster_sm, btm_monster_vga;
 	reg btm_monster_ctrl;  
 	wire left_monster, right_monster; 
+	wire top_broken, btm_broken, left_broken, right_broken;
 	wire right_shield, left_shield; 
-	wire top_random, btm_random, left_random, right_random;
-	wire TR_random, BR_random, LR_random, RR_random;
-	wire [3:0] TR_combo, BR_combo, LR_combo, RR_combo;
 	wire top_gameover, btm_gameover, left_gameover, right_gameover;
 	reg gameover_ctrl;  
-	reg [3:0] hex_combo;
-	wire [3:0] random_hex;
-	reg TR_submit;
+	
+	// Random probability
+	wire top_random, btm_random, left_random, right_random;
+	wire TR_random, BR_random, LR_random, RR_random;
+	
+	// Hex combinations for repairs
+	wire [3:0] TR_combo, BR_combo, LR_combo, RR_combo;
+	reg [3:0] hex_combo; // current hex combination on Sw0-Sw3
+	wire [3:0] random_hex; // currently produced random hex
+	
+	// Game timer
+	reg [3:0] game_time_min;
+	reg [5:0] game_time_sec_1s;
+	reg [5:0] game_time_sec_10s;
+	reg [18:0] clk_cycle_count;
+
+	// SSDs
 	reg [3:0]	SSD;
 	wire [7:0]	SSD7, SSD6, SSD5, SSD4, SSD3, SSD2, SSD1, SSD0;
 	reg [7:0]  SSD_CATHODES;
 	
-	
-	
 
-	
 //------------	
 // Disable the three memories so that they do not interfere with the rest of the design.
 	assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
-	
 	
 //------------
 // CLOCK DIVISION
@@ -139,72 +134,54 @@ module nexys_starship_top
 	// Our clock is too fast (100MHz) for SSD scanning
 	// create a series of slower "divided" clocks
 	// each successive bit is 1/2 frequency
-  always @(posedge board_clk, posedge Reset) 	
-    begin							
-        if (Reset)
+	always @(posedge board_clk, posedge Reset) 	
+	begin							
+		if (Reset)
 		DIV_CLK <= 0;
-        else
+		else
 		DIV_CLK <= DIV_CLK + 1'b1;
-    end
+	end
 //-------------------	
 	// In this design, we run the core design at full 100MHz clock!
 	assign	sys_clk = board_clk;
 
 //------------------
-	// CLOCK
+	// CLOCKS
 	assign move_clk = DIV_CLK[19]; //slower clock to drive the movement of objects on the vga screen
 	assign random_clk = DIV_CLK[24];
 	assign timer_clk = DIV_CLK[24];
 	assign game_timer_clk = DIV_CLK[7];
 
 //------------
-// INPUT: SWITCHES & BUTTONS
+// INPUT: BUTTONS
 
 	assign {Up_Pulse, Down_Pulse, Left_Pulse, Right_Pulse, Center_Pulse} = 
 	        {BtnU_Pulse, BtnD_Pulse, BtnL_Pulse, BtnR_Pulse, BtnC_Pulse};
-	      
 
-ee354_debouncer #(.N_dc(28)) ee354_debouncer_0 
-        (.CLK(sys_clk), .RESET(Reset), .PB(BtnR), .DPB( ), 
-		.SCEN(BtnR_Pulse), .MCEN( ), .CCEN( ));
+	ee354_debouncer #(.N_dc(28)) ee354_debouncer_0 
+			(.CLK(sys_clk), .RESET(Reset), .PB(BtnR), .DPB( ), 
+			.SCEN(BtnR_Pulse), .MCEN( ), .CCEN( ));
 
-ee354_debouncer #(.N_dc(28)) ee354_debouncer_1 
-        (.CLK(sys_clk), .RESET(Reset), .PB(BtnU), .DPB( ), 
-		.SCEN(BtnU_Pulse), .MCEN( ), .CCEN( )); // to produce BtnU_Pulse from BtnU
-		
-ee354_debouncer #(.N_dc(28)) ee354_debouncer_2 
-        (.CLK(sys_clk), .RESET(Reset), .PB(BtnL), .DPB( ), 
-		.SCEN(BtnL_Pulse), .MCEN( ), .CCEN( ));
+	ee354_debouncer #(.N_dc(28)) ee354_debouncer_1 
+			(.CLK(sys_clk), .RESET(Reset), .PB(BtnU), .DPB( ), 
+			.SCEN(BtnU_Pulse), .MCEN( ), .CCEN( )); // to produce BtnU_Pulse from BtnU
+			
+	ee354_debouncer #(.N_dc(28)) ee354_debouncer_2 
+			(.CLK(sys_clk), .RESET(Reset), .PB(BtnL), .DPB( ), 
+			.SCEN(BtnL_Pulse), .MCEN( ), .CCEN( ));
 
-ee354_debouncer #(.N_dc(28)) ee354_debouncer_3 
-        (.CLK(sys_clk), .RESET(Reset), .PB(BtnD), .DPB( ), 
-		.SCEN(BtnD_Pulse), .MCEN( ), .CCEN( ));
-		
-ee354_debouncer #(.N_dc(28)) ee354_debouncer_4 
-        (.CLK(sys_clk), .RESET(Reset), .PB(BtnC), .DPB( ), 
-		.SCEN(BtnC_Pulse), .MCEN( ), .CCEN( ));
+	ee354_debouncer #(.N_dc(28)) ee354_debouncer_3 
+			(.CLK(sys_clk), .RESET(Reset), .PB(BtnD), .DPB( ), 
+			.SCEN(BtnD_Pulse), .MCEN( ), .CCEN( ));
+			
+	ee354_debouncer #(.N_dc(28)) ee354_debouncer_4 
+			(.CLK(sys_clk), .RESET(Reset), .PB(BtnC), .DPB( ), 
+			.SCEN(BtnC_Pulse), .MCEN( ), .CCEN( ));
 	
 //------------
 // DESIGN
 
-	always @ (posedge sys_clk, posedge Reset)
-	begin
-		if(Reset)
-		begin
-	        hex_combo <= 4'b0000; 
-	        TR_submit <= 0;
-		end
-		else
-		begin
-			//if (Up_Pulse)  		
-				//begin	
-					hex_combo <= {Sw3, Sw2, Sw1, Sw0};	
-					//TR_submit <= 1;
-				//end
-		end
-	end
-	
-	// the state machine modules
+	// State machine modules
 	nexys_starship_game nexys_starship_game_1(.Clk(sys_clk), .BtnC(Center_Pulse),
 	                      .BtnU(Up_Pulse), .Reset(Reset), .q_Init(q_Init),
 	                      .q_Play(q_Play), .q_GameOver(q_GameOver), 
@@ -235,95 +212,101 @@ ee354_debouncer #(.N_dc(28)) ee354_debouncer_4
                           .gameover_ctrl(gameover_ctrl), .timer_clk(timer_clk));
 					  
 	nexys_starship_TR nexys_starship_TR_1(.Clk(sys_clk), .Reset(Reset), .q_TR_Init(q_TR_Init), 
-	                       .q_TR_Working(q_TR_Working), .q_TR_Repair(q_TR_Repair), .BtnU(Up_Pulse),
-                           .play_flag(play_flag), .top_broken(top_broken), .hex_combo(hex_combo), 
-                           .random_hex(random_hex), .gameover_ctrl(gameover_ctrl),
-                           .TR_random(TR_random), .BtnR(Right_Pulse),
-                           .TR_combo(TR_combo), .timer_clk(timer_clk));
+	                      .q_TR_Working(q_TR_Working), .q_TR_Repair(q_TR_Repair), .BtnU(Up_Pulse),
+                          .play_flag(play_flag), .top_broken(top_broken), .hex_combo(hex_combo), 
+                          .random_hex(random_hex), .gameover_ctrl(gameover_ctrl),
+                          .TR_random(TR_random), .TR_combo(TR_combo), .timer_clk(timer_clk));
                             
 	nexys_starship_BR nexys_starship_BR_1(.Clk(sys_clk), .Reset(Reset), .q_BR_Init(q_BR_Init), 
-	                       .q_BR_Working(q_BR_Working), .q_BR_Repair(q_BR_Repair), .BtnD(Down_Pulse),
-                           .play_flag(play_flag), .btm_broken(btm_broken), .hex_combo(hex_combo), 
-                           .random_hex(random_hex), .gameover_ctrl(gameover_ctrl),
-                           .BR_random(BR_random), .BtnR(Right_Pulse),
-                           .BR_combo(BR_combo), .timer_clk(timer_clk));                      
+	                      .q_BR_Working(q_BR_Working), .q_BR_Repair(q_BR_Repair), .BtnD(Down_Pulse),
+                          .play_flag(play_flag), .btm_broken(btm_broken), .hex_combo(hex_combo), 
+                          .random_hex(random_hex), .gameover_ctrl(gameover_ctrl),
+                          .BR_random(BR_random), .BR_combo(BR_combo), .timer_clk(timer_clk));                      
                            
     nexys_starship_LR nexys_starship_LR_1(.Clk(sys_clk), .Reset(Reset), .q_LR_Init(q_LR_Init), 
-	                       .q_LR_Working(q_LR_Working), .q_LR_Repair(q_LR_Repair), .BtnL(Left_Pulse),
-                           .play_flag(play_flag), .left_broken(left_broken), .hex_combo(hex_combo), 
-                           .random_hex(random_hex), .gameover_ctrl(gameover_ctrl),
-                           .LR_random(LR_random), .BtnR(Right_Pulse),
-                           .LR_combo(LR_combo), .timer_clk(timer_clk)); 
+	                      .q_LR_Working(q_LR_Working), .q_LR_Repair(q_LR_Repair), .BtnL(Left_Pulse),
+                          .play_flag(play_flag), .left_broken(left_broken), .hex_combo(hex_combo), 
+                          .random_hex(random_hex), .gameover_ctrl(gameover_ctrl),
+                          .LR_random(LR_random), .LR_combo(LR_combo), .timer_clk(timer_clk)); 
     
     nexys_starship_RR nexys_starship_RR_1(.Clk(sys_clk), .Reset(Reset), .q_RR_Init(q_RR_Init), 
-	                       .q_RR_Working(q_RR_Working), .q_RR_Repair(q_RR_Repair), .BtnR(Right_Pulse),
-                           .play_flag(play_flag), .right_broken(right_broken), .hex_combo(hex_combo), 
-                           .random_hex(random_hex), .gameover_ctrl(gameover_ctrl),
-                           .RR_random(RR_random), .RR_combo(RR_combo), .timer_clk(timer_clk));        
+	                      .q_RR_Working(q_RR_Working), .q_RR_Repair(q_RR_Repair), .BtnR(Right_Pulse),
+                          .play_flag(play_flag), .right_broken(right_broken), .hex_combo(hex_combo), 
+                          .random_hex(random_hex), .gameover_ctrl(gameover_ctrl),
+                          .RR_random(RR_random), .RR_combo(RR_combo), .timer_clk(timer_clk));        
 				  
-	// random modules
+	// Pseudo-random number generator module
 	nexys_starship_PRNG nexys_starship_PRNG_1(.Clk(random_clk), .Reset(Reset),
-	                    .top_random(top_random), .btm_random(btm_random), .left_random(left_random),
-                        .right_random(right_random), .TR_random(TR_random), .BR_random(BR_random),
-                        .LR_random(LR_random), .RR_random(RR_random), .random_hex(random_hex));
+	                     .top_random(top_random), .btm_random(btm_random), .left_random(left_random),
+                         .right_random(right_random), .TR_random(TR_random), .BR_random(BR_random),
+                         .LR_random(LR_random), .RR_random(RR_random), .random_hex(random_hex));
                       
-	
-	// vga modules
+	// VGA modules
 	display_controller dc(.Clk(sys_clk), .hSync(hSync), .vSync(vSync), .bright(bright), .hCount(hc), 
-	                    .vCount(vc));
+	                     .vCount(vc));
 	
 	block_controller sc(.Clk(move_clk), .bright(bright), .Reset(Reset), .up(BtnU), .down(BtnD),
-	                       .left(BtnL), .right(BtnR), .hCount(hc), .vCount(vc), .rgb(rgb),
-	                       .top_monster_vga(top_monster_vga), .top_monster_ctrl(top_monster_ctrl), 
-	                       .top_broken(top_broken), .btm_monster_vga(btm_monster_vga), 
-	                       .btm_monster_ctrl(btm_monster_ctrl), .btm_broken(btm_broken),
-	                       .left_monster(left_monster), .left_shield(left_shield), 
-	                       .left_broken(left_broken), .right_monster(right_monster),
-	                       .right_shield(right_shield), .right_broken(right_broken),
-	                       .sysClk(sys_clk), .TR_combo(TR_combo), .BR_combo(BR_combo),
-	                       .LR_combo(LR_combo), .RR_combo(RR_combo), .play_flag(play_flag),
-	                       .gameover_ctrl(gameover_ctrl));
-	                       
+						 .left(BtnL), .right(BtnR), .hCount(hc), .vCount(vc), .rgb(rgb),
+						 .top_monster_vga(top_monster_vga), .top_monster_ctrl(top_monster_ctrl), 
+						 .top_broken(top_broken), .btm_monster_vga(btm_monster_vga), 
+						 .btm_monster_ctrl(btm_monster_ctrl), .btm_broken(btm_broken),
+						 .left_monster(left_monster), .left_shield(left_shield), 
+						 .left_broken(left_broken), .right_monster(right_monster),
+						 .right_shield(right_shield), .right_broken(right_broken),
+						 .sysClk(sys_clk), .TR_combo(TR_combo), .BR_combo(BR_combo),
+						 .LR_combo(LR_combo), .RR_combo(RR_combo), .play_flag(play_flag),
+						 .gameover_ctrl(gameover_ctrl));
 	                       
 //------------
-// GAME TIMER
-reg [3:0] game_time_min;
-reg [5:0] game_time_sec_1s;
-reg [5:0] game_time_sec_10s;
-reg [18:0] clk_cycle_count;
-always @ (posedge game_timer_clk, posedge Reset)
-begin
-    if (Reset)
-    begin
-        game_time_min <= 0;
-        game_time_sec_1s <= 0;
-        game_time_sec_10s <= 0;
-        clk_cycle_count <= 0;
-    end
-    else if (play_flag)
-    begin
-        // game_timer <= game_timer + 1;
-        clk_cycle_count <= clk_cycle_count + 1;
-        if (clk_cycle_count == 390624)
-        begin
-            clk_cycle_count <= 0;
-            game_time_sec_1s <= game_time_sec_1s + 1;
-            if (game_time_sec_1s == 9)
-            begin
-                game_time_sec_1s <= 0;
-                game_time_sec_10s <= game_time_sec_10s + 1;
-            end
-            if (game_time_sec_10s == 5)
-            begin
-                game_time_sec_10s <= 0;
-                game_time_min <= game_time_min + 1;
-            end
-        end
-    end
-end
+// SWITCHES HEX COMBO
+
+	always @ (posedge sys_clk, posedge Reset)
+	begin
+		if(Reset)
+		begin
+	        hex_combo <= 4'b0000; 
+		end
+		else
+		begin
+			hex_combo <= {Sw3, Sw2, Sw1, Sw0};	
+		end
+	end
 
 //------------
-// SHARED REGISTERS 
+// GAME TIMER
+
+	always @ (posedge game_timer_clk, posedge Reset)
+	begin
+		if (Reset)
+		begin
+			game_time_min <= 0;
+			game_time_sec_1s <= 0;
+			game_time_sec_10s <= 0;
+			clk_cycle_count <= 0;
+		end
+		else if (play_flag)
+		begin
+			clk_cycle_count <= clk_cycle_count + 1;
+			if (clk_cycle_count == 390624)
+			begin
+				clk_cycle_count <= 0;
+				game_time_sec_1s <= game_time_sec_1s + 1;
+				if (game_time_sec_1s == 9)
+				begin
+					game_time_sec_1s <= 0;
+					game_time_sec_10s <= game_time_sec_10s + 1;
+				end
+				if (game_time_sec_10s == 5)
+				begin
+					game_time_sec_10s <= 0;
+					game_time_min <= game_time_min + 1;
+				end
+			end
+		end
+	end
+
+//------------
+// SHARED REGISTERS
 
     always @ (*)
     begin
@@ -331,18 +314,12 @@ end
             top_monster_ctrl <= top_monster_vga;
         else
             top_monster_ctrl <= top_monster_sm;  
-    end 
-    
-    always @ (*)
-    begin
+			
         if (q_BM_Full)
             btm_monster_ctrl <= btm_monster_vga; 
         else
             btm_monster_ctrl <= btm_monster_sm;  
-    end 
-    
-    always @ (*)
-    begin 
+			
         if (q_GameOver)
             gameover_ctrl <= 1; 
         else
@@ -351,28 +328,27 @@ end
 
 //------------
 // VGA OUTPUT
+
 	assign vgaR = rgb[11 : 8];
 	assign vgaG = rgb[7  : 4];
 	assign vgaB = rgb[3  : 0];
 
 //------------
 // OUTPUT: LEDS
-	
+
 	assign {Ld7, Ld6, Ld5, Ld4} = {q_LM_Init, q_LM_Full, q_LM_Empty, left_monster};
 	assign {Ld3, Ld2, Ld1, Ld0} = {left_shield, q_GameOver, left_gameover, gameover_ctrl}; // Reset is driven by BtnC
 
 //------------
 // SSD (Seven Segment Display)
 
-	assign SSD0 = {Sw3, Sw2, Sw1, Sw0};
-	assign SSD1 = BR_combo;
+	assign SSD0 = {Sw3, Sw2, Sw1, Sw0}; // current hex combo
 	assign SSD4 = game_time_sec_1s;
 	assign SSD5 = game_time_sec_10s;
 	assign SSD6 = game_time_min;
 
 
-	// need a scan clk for the seven segment display 
-	// 191Hz (100 MHz / 2^19) works well
+	// scan clk for the seven segment display 
 	assign ssdscan_clk = DIV_CLK[16:14];
 	
     assign An0    = ~(~(ssdscan_clk[2]) && ~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when sev_seg_clk = 000
@@ -407,24 +383,24 @@ end
 	// Following is Hex-to-SSD conversion
 	always @ (SSD) 
 	begin : HEX_TO_SSD
-		case (SSD) // in this solution file the dot points are made to glow by making Dp = 0
-		    //                                                                abcdefg,Dp
-			4'b0000: SSD_CATHODES = 8'b00000010; // 0
-			4'b0001: SSD_CATHODES = 8'b10011110; // 1
-			4'b0010: SSD_CATHODES = 8'b00100100; // 2
-			4'b0011: SSD_CATHODES = 8'b00001100; // 3
-			4'b0100: SSD_CATHODES = 8'b10011000; // 4
-			4'b0101: SSD_CATHODES = 8'b01001000; // 5
-			4'b0110: SSD_CATHODES = 8'b01000000; // 6
-			4'b0111: SSD_CATHODES = 8'b00011110; // 7
-			4'b1000: SSD_CATHODES = 8'b00000000; // 8
-			4'b1001: SSD_CATHODES = 8'b00001000; // 9
-			4'b1010: SSD_CATHODES = 8'b00010000; // A
-			4'b1011: SSD_CATHODES = 8'b11000000; // B
-			4'b1100: SSD_CATHODES = 8'b01100010; // C
-			4'b1101: SSD_CATHODES = 8'b10000100; // D
-			4'b1110: SSD_CATHODES = 8'b01100000; // E
-			4'b1111: SSD_CATHODES = 8'b01110000; // F    
+		case (SSD)
+		    // abcdefg,Dp
+			4'b0000: SSD_CATHODES = 8'b00000011; // 0
+			4'b0001: SSD_CATHODES = 8'b10011111; // 1
+			4'b0010: SSD_CATHODES = 8'b00100101; // 2
+			4'b0011: SSD_CATHODES = 8'b00001101; // 3
+			4'b0100: SSD_CATHODES = 8'b10011001; // 4
+			4'b0101: SSD_CATHODES = 8'b01001001; // 5
+			4'b0110: SSD_CATHODES = 8'b01000001; // 6
+			4'b0111: SSD_CATHODES = 8'b00011111; // 7
+			4'b1000: SSD_CATHODES = 8'b00000001; // 8
+			4'b1001: SSD_CATHODES = 8'b00001001; // 9
+			4'b1010: SSD_CATHODES = 8'b00010001; // A
+			4'b1011: SSD_CATHODES = 8'b11000001; // B
+			4'b1100: SSD_CATHODES = 8'b01100011; // C
+			4'b1101: SSD_CATHODES = 8'b10000101; // D
+			4'b1110: SSD_CATHODES = 8'b01100001; // E
+			4'b1111: SSD_CATHODES = 8'b01110001; // F    
 			default: SSD_CATHODES = 8'bXXXXXXXX; // default is not needed as we covered all cases
 		endcase
 	end	
